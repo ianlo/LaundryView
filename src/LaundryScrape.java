175303f9adcc9e6242c0832da38bb8833841f5cd
Ian Lo <ianlo196@gmail.com>
2015-12-07 00:49:40 -0500

    Scraping works

--- src/LaundryScrape.java
+++ src/LaundryScrape.java
@@ -19,13 +19,52 @@ public class LaundryScrape {
 			HtmlTableRow table = ((HtmlTableRow) ((HtmlTable) monitor.getFirstChild()).getRow(0));
 			HtmlTable left = (HtmlTable) table.getCell(0).getFirstChild();
 			HtmlTable right = (HtmlTable) table.getCell(1).getFirstChild();
-			System.out.println(left.asXml());
-			//TODO iterate through the table and generate Machine objects for each machine.
+			int numWashers = left.getRowCount()/2;
+			int numDryers = left.getRowCount()/2;
 			
+			Machine[] washers = new Machine[numWashers];
+			Machine[] dryers = new Machine[numDryers];
+			
+			for(int i = 0; i < numWashers; i++) {
+				//The first row contains the machine number.
+				Machine m = new Machine(Machine.WASHER, Integer.parseInt(left.getRow(2 * i).getCell(1).asText()));
+				//The text for the machine is on the next row.
+				setStatus(m, left.getRow(2 * i + 1).asText());
+				washers[i] = m;
+			}
+			
+			for(int i = 0; i < numDryers; i++) {
+				//The first row contains the machine number.
+				Machine m = new Machine(Machine.DRYER, Integer.parseInt(left.getRow(2 * i).getCell(1).asText()));
+				//The text for the machine is on the next row.
+				setStatus(m, left.getRow(2 * i + 1).asText());
+				dryers[i] = m;
+			}
+
 		} catch (FailingHttpStatusCodeException | IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+	public static Machine setStatus(Machine m, String machineText) {
+		//Set the status of the machines based on what is in the scraped text.
+		if (machineText.contains("time remaining")) {
+			m.setStatus(Machine.RUNNING);
+			//Scrape the time remaining from the text and set it to the machine's time remaining.
+			int startIndex = machineText.indexOf("remaining") + 10;
+			int endIndex = machineText.indexOf("min") - 1;
+			m.setTimeRemaining(Integer.parseInt(machineText.substring(startIndex, endIndex)));
+		} 
+		else if (machineText.contains("out of service")) {
+			m.setStatus(Machine.OUTOFSERVICE);
+		}
+		else if (machineText.contains("ended")) {
+			m.setStatus(Machine.ENDED);
+		}
+		else {
+			m.setStatus(Machine.AVAILABLE);
+		}
+		return m;
 	}
 
 }
--- src/Machine.java
+++ src/Machine.java
@@ -13,6 +13,7 @@ public class Machine {
 	//Local fields.
 	private int type;
 	private int status;
+	private int number;
 	
 	//Time remaining in minutes.
 	private int timeRemaining;
@@ -20,12 +21,39 @@ public class Machine {
 	 * Creates a new laundry machine.
 	 * @param type one of the type constants given by this class (i.e. WASHER, DRYER).
 	 * @param status one of the status constants given by this class (i.e. AVAILABLE, RUNNING, etc.).
+	 * @param number the laundry machine number that is written on the machine.
 	 */
-	public Machine(int type, int status){
+	public Machine(int type,  int number){
 		this.type = type;
-		this.status = status;
+		this.status = AVAILABLE;
+		this.number = number;
 	}
 	
-	//TODO add methods for changing the status and setting the time remaining on the machine.
+	public int getType() {
+		return type;
+	}
 	
+	public int getStatus() {
+		return status;
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
+		if (status != RUNNING) timeRemaining = 0;
+	}
+	
+	public int getTimeRemaining() {
+		return timeRemaining;
+	}
+	
+	//When you set the time remaining, it automatically sets the machine to running.
+	public void setTimeRemaining(int timeRemaining) {
+		this.timeRemaining = timeRemaining;
+		this.status = RUNNING;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+		
 }
