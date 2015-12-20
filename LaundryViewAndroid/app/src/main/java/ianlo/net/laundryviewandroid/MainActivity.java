6ee411e5d8350e7a84ea2ba2e50d1e957ce46c8d
Ian Lo <ianlo196@gmail.com>
2015-12-20 13:32:08 -0500

    Made status bar coloured

--- LaundryViewAndroid/app/src/main/java/ianlo/net/laundryviewandroid/MainActivity.java
+++ LaundryViewAndroid/app/src/main/java/ianlo/net/laundryviewandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -77,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         // Remove the shadow on the action bar.
         getSupportActionBar().setElevation(0);
 
