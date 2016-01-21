1604a5b68319a9c1cf4f310e1b7f00a904135cf9
Ian Lo <ianlo196@gmail.com>
2016-01-21 00:11:24 -0500

    Notification is removed from PendingIntents after it's triggered.

--- LaundryViewAndroid/app/src/main/java/ianlo/net/laundryviewandroid/NotificationReceiver.java
+++ LaundryViewAndroid/app/src/main/java/ianlo/net/laundryviewandroid/NotificationReceiver.java
@@ -1,11 +1,13 @@
 package ianlo.net.laundryviewandroid;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.support.v7.app.NotificationCompat;
 
 import ui.MainActivity;
@@ -22,10 +24,17 @@ public class NotificationReceiver extends BroadcastReceiver {
                 .setContentTitle("Laundry Cycle Finished")
                 .setContentText("Go pick up your laundry!")
                 .setSmallIcon(R.drawable.abc_ic_go_search_api_mtrl_alpha)
+                .setVibrate(new long[] {500})
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                 .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0))
                 .build();
 
         notificationManager.notify(0, notification);
+        // Get the PendingIntent and then cancel the scheduled notification.
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_NO_CREATE);
+        alarmMgr.cancel(alarmIntent);
+        alarmIntent.cancel();
     }
 }
 
