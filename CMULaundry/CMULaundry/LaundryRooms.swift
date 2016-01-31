31abb03b81c6c35c924db352d6b2097a5f45edd2
Ian Lo <ianlo196@gmail.com>
2016-01-30 23:07:38 -0500

    Started iOS code

---
+++ CMULaundry/CMULaundry/LaundryRooms.swift
@@ -0,0 +1,18 @@
+//
+//  LaundryRooms.swift
+//  CMULaundry
+//
+//  Created by Ian Lo on 2016-01-29.
+//  Copyright © 2016 Ian Lo. All rights reserved.
+//
+
+import Foundation
+
+class LaundryRoom {
+    var name: String
+    var url: String
+    init(name: String, url: String) {
+        self.name = name
+        self.url = url
+    }
+}
