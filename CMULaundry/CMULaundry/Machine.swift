485f7ff84bec46f1696b7a654f884b2c89a6c212
Ian Lo <ianlo196@gmail.com>
2016-02-04 19:57:58 -0500

    Added setStatusWithString to Machine

--- CMULaundry/CMULaundry/Machine.swift
+++ CMULaundry/CMULaundry/Machine.swift
@@ -22,16 +22,23 @@ class Machine {
     
     // Local fields.
     var type: Int
-    var status: Int = 0 {
+    var status: Int {
         didSet {
+            // If the machine isn't running, reset the timeRemaining.
             if (self.status != Machine.RUNNING) {
                 timeRemaining = 0
             }
         }
     }
     var number: Int
-    var timeRemaining = 0
+    var timeRemaining = 0 {
+        didSet {
+            // If the machine has time remaining, set it to running.
+            self.status = Machine.RUNNING
+        }
+    }
     
+    // Constructor for the Machine class.
     init(type: Int, status: Int, number: Int) {
         self.type = type
         self.status = status
@@ -52,6 +59,37 @@ class Machine {
             return "Status Unknown"
         default:
             return "Status Unknown"
+        }
+    }
+    
+    func setStatusWithString(machineText : String) {
+        // Set the status of the machines based on what is in the scraped text.
+        if (machineText.rangeOfString("available") != nil) {
+            self.status = Machine.AVAILABLE;
+        }
+        else if (machineText.rangeOfString("time remaining") != nil) {
+            self.status = Machine.RUNNING;
+            // Scrape the time remaining from the text and set it to the machine's time remaining.
+            let startRange : Range? = machineText.rangeOfString("remaining");
+            let endRange : Range? = machineText.rangeOfString("min");
+            if (startRange != nil && endRange != nil) {
+                let startIndex = startRange!.startIndex.advancedBy(10);
+                let endIndex = endRange!.startIndex.advancedBy(-1);
+                let newRange = Range(start: startIndex, end: endIndex);
+                timeRemaining = Int(machineText.substringWithRange(newRange))!;
+            }
+        }
+        else if (machineText.rangeOfString("out of service") != nil) {
+            self.status = Machine.OUTOFSERVICE;
+        }
+        else if (machineText.rangeOfString("ended") != nil) {
+            self.status = Machine.ENDED;
+        }
+        else if (machineText.rangeOfString("unknown") != nil) {
+            self.status = Machine.UNKNOWN;
+        }
+        else {
+            self.status = Machine.UNKNOWN;
         }
     }
 }
