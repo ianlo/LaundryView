bf1327c580710aa3149e176444b44663dbd55e25
Ian Lo <ianlo196@gmail.com>
2016-02-06 17:03:40 -0500

    Small comments

--- CMULaundry/CMULaundry/LaundryRooms.swift
+++ CMULaundry/CMULaundry/LaundryRooms.swift
@@ -9,7 +9,9 @@
 import Foundation
 
 class LaundryRoom {
+    // The location of the LaundryRoom.
     var name: String
+    // The URL containg the data for that location.
     var url: String
     init(name: String, url: String) {
         self.name = name
